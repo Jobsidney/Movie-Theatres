98be143053ec2291677ab79b030faa7c959d7abf
Jobsidney <jobsidney67@gmail.com>
2022-06-22 01:49:34 +0300

    Movie cards from API

--- assets/Js/index.js
+++ assets/Js/index.js
@@ -10,6 +10,60 @@ function movieData(url){
     .then(response => response.json())
     .then(data => {
         console.log(data.results);
-        data.results})
+        displayCard(data.results)
+        
+    })
 }
 movieData(apiURL)
+
+function displayCard(data){
+    let movieSection=document.querySelector('#movieID')
+    movieSection.innerHTML=''
+    data.forEach(movie => {
+        console.log(movie);
+        const {title,poster_path,vote_average,overview}=movie
+        const card = document.createElement('div')
+        card.className='card'
+        card.innerHTML=`
+        <img src="${imageBaseUrl}${poster_path}" alt="">
+        
+        <div class="overview">
+            <div class="Movietitle">
+                <div>${title}</div>
+                <div>${vote_average}</div>
+            </div>
+            <h3>OVERVIEW</h3>
+            <p>
+            ${overview}</p>
+            
+        </div>
+        `
+        movieSection.appendChild(card)
+
+        
+    });
+}
+
+// console.log(data.results);
+// data.results.foreach(item => {
+//     const movieSection=document.querySelector('.movie-cards')
+//     movieSection.innerHTML=`
+//     <div class="card">
+//     <img src="${imageBaseUrl}${item.poster_path}" alt="">
+    
+//     <div class="overview">
+//         <div class="Movietitle">
+//             <div>${item.title}</div>
+//             <div>${item.vote_average}</div>
+//         </div>
+//         <h3>OVERVIEW</h3>
+//         <p>
+//         ${item.overview}</p>
+//         </p>
+//     </div>
+
+
+// </div>
+
+//     `
+// })
