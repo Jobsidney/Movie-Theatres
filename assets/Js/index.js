ab7474bb87fcb30a5c5487c7915a7050aed8657f
Jobsidney <jobsidney67@gmail.com>
2022-06-23 01:17:21 +0300

    News sections cards and API

--- assets/Js/index.js
+++ assets/Js/index.js
@@ -10,6 +10,11 @@ const scienceFixApi=baseUrl+"/discover/movie?with_genres=878&with_cast=500&sort_
 const upcomingAPI=baseUrl+"/discover/movie?primary_release_date.gte=2014-09-15&primary_release_date.lte=2024-10-22&"+apiKey
 const dramaApi=baseUrl+"/discover/movie?with_genres=18&sort_by=vote_average.desc&vote_count.gte=10&"+apiKey
 const highestRatedMOvieApi=baseUrl+"/discover/movie/?certification_country=US&certification=R&sort_by=vote_average.desc&"+apiKey
+
+
+const newsKey='https://newsapi.org/v2/everything?q=actors&from=2022-05-22&sortBy=publishedAt&apiKey=a175074b2bb84dd0a7dac3a9f7ac007e';
+
+
 function movieData(url){
     fetch(url)
     .then(response => response.json())
@@ -19,17 +24,7 @@ function movieData(url){
         
     })
 }
-function movieKids(url){
-    fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data.results);
-        displayCard(data.results)
-        
-    })
 
-}
-// movieKids(kidsAPI)
 movieData(upcomingAPI)
 
 function displayCard(data){
@@ -48,7 +43,7 @@ function displayCard(data){
                 <div class="title">${title}</div>
                 <div>${vote_average}</div>
             </div>
-            <h3>OVERVIEW</h3>
+            <h4>OVERVIEW</h4>
             <p>
             ${overview}</p>
             
@@ -109,5 +104,50 @@ callSeries.addEventListener('click',function(){  if (true) {
     let movieSection=document.querySelector('#movieID')
     movieSection.innerHTML=''
   }})
+
+
+
+//NEWS AND BLOG SECTIONS
+
+
+  function newsBlogs(url){
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data.articles);
+        displayNews(data.articles);
+        
+    })
+
+}
+newsBlogs(newsKey)
+  function displayNews(data){
+    let newsSection=document.querySelector('#newsTemplates')
+    newsSection.innerHTML=''
+    for (let i = 17; i< 22; i++) {
+      const element = data[i];
+      console.log(data[i]);
+      const {title,urlToImage,content,}=element
+      const card = document.createElement('div')
+      card.className='card'
+      card.innerHTML=`
+      <img src="${urlToImage}" alt="">
+      
+      <div class="overview">
+          <div class="Movietitle">
+              <div class="title">${title}</div>
+          </div>
+          <p>
+          ${content}</p>
+          
+      </div>
+      `
+      newsSection.appendChild(card)
+
+      
+  
+     
+    }
+}
 
   
